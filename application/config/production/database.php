4f93e48465aa760397f8ca0c7dbd624af6a9e696
Ankur rana <ankurofficial@hotmail.com>
2013-11-03 17:51:49 +0530

    First Commit

---
+++ application/config/production/database.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+	'connections' => array(
+		'mysql' => array(
+			'driver'   => 'mysql',
+			'host'     => 'localhost',
+			'database' => 'coderecipe',
+			'username' => 'root',
+			'password' => '',
+			'charset'  => 'utf8',
+			'prefix'   => ''
+		)
+	)
+)
